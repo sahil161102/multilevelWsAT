956b1986609ce99476e83946c006b253a14ab927
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2022-03-29 07:33:40 +0530

    Update 4.2.c

--- 4.2.c
+++ 4.2.c
@@ -1,57 +1,78 @@
 #include<stdio.h>
-int p[20],ptype[20],flag[20],at[20], bt[20], wt[20], tat[20], i, k,n,time=0, temp;
-int sjfSort(){
-for(i=0;i<n && at[i] && flag[i]!= 1<=time;i++)
-for(k=i+1;k<n && at[i] && flag[i]!= 1<=time;k++)
-if(bt[i]>bt[k])
+#include <limits.h>
+struct readyQueue{
+    int pid;
+    int type;
+    int bt;
+    int at;
+};
+int main()
 {
-temp=bt[i];
-bt[i]=bt[k];
-bt[k]=temp;
-
-temp=p[i];
-p[i]=p[k];
-p[k]=temp;
-}
-  
-temp=ptype[i];
-ptype[i]=ptype[k];
-ptype[k]=temp;
-}
-return 0;
-}
-
-void main()
-{
-float cwt, ctat;
-printf("\nEnter the number of procesess-- ");
-scanf("%d", &n);
-for(i=0;i<n;i++)
-{
-p[i]=i;
-printf("Enter 0 for system p and 1 for user p --", i);
-scanf("%d", &ptype[i]);
-printf("Enter Arrival Time for Process %d -- ", i);
-scanf("%d", &at[i]);
-printf("Enter Burst Time for Process %d -- ", i);
-scanf("%d", &bt[i]);
-  flag[i] = 0;
-}
-for(int i =0;i<
-
-wt[0] = cwt = 0;
-tat[0] = ctat = bt[0];
-for(i=1;i<n;i++)
-{
-wt[i] = wt[i-1] +bt[i-1];
-tat[i] = wt[i] +bt[i] ;
-cwt = cwt + wt[i];
-ctat = ctat + tat[i];
-}
-printf("\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
-for(i=0;i<n;i++)
-printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
-printf("\nAverage Waiting Time -- %f", cwt/n);
-printf("\nAverage Turnaround Time -- %f", ctat/n);
-
+    int n,i_time=-1,time=0,index=0,pCount=0;
+    float cwt=0,ctat=0;
+    printf("Enter the number of processes:\t");
+    scanf("%d",&n);
+    int AT[n],pType[n],BT[n];
+    for(int i=0;i<n;i++){
+        printf("Enter the type of the process%d: (0-system 1-user) :\t",i);
+        scanf("%d",&pType[i]);
+        printf("Enter the Arrival time of the process %d:\t",i);
+        scanf("%d",&AT[i]);
+        printf("Enter the Burst time of the process %d: \t",i);
+        scanf("%d",&BT[i]);
+        
+    }
+    
+   
+    struct readyQueue RQ[n];
+    
+        printf("\nPROCESS\t\t ARRIVAL TIME BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
+        int N=n;
+    while(N--){
+        for(int i=0;i<n;i++){
+            if(AT[i]>i_time && AT[i]<=time){
+                RQ[index].type=pType[i];
+                RQ[index].pid=i;
+                RQ[index].bt=BT[i];
+                RQ[index].at=AT[i];
+                index++;
+                
+            }
+        }
+        i_time=time;
+        int min=999,min_id=-1;
+        for(int i=0;i<index;i++){
+            if(RQ[i].bt<min && RQ[i].type==0 && RQ[i].bt>0){
+                min=RQ[i].bt;
+                min_id=i;
+            }
+        }
+        
+        if(min_id!=-1){
+            pCount++;
+            time+=RQ[min_id].bt;
+            ctat=time -RQ[min_id].at;
+            cwt=ctat - RQ[min_id].bt;
+            printf("%d\t\t %d\t\t %d\t\t %f\t\t %f \n",RQ[min_id].pid,RQ[min_id].at,min, cwt,ctat  );
+            RQ[min_id].bt=-1;
+        }else if(pCount<index){
+            int min=999,min_id=-1;
+            for(int i=0;i<index;i++){
+                if(RQ[i].bt>0 && RQ[i].type==1 && RQ[i].at<min){
+                    min=RQ[i].at;
+                    min_id=i;
+                }
+            }
+            pCount++;
+            time+=RQ[min_id].bt;
+            ctat=time - RQ[min_id].at;
+            cwt=ctat-RQ[min_id].bt;
+            printf("%d\t\t %d\t\t %d\t\t %f\t\t %f \n",RQ[min_id].pid,RQ[min_id].at,RQ[min_id].bt, cwt,ctat  );
+            RQ[min_id].bt=-1;
+        }else{
+            time++;
+            N++;
+        }
+    }
+    return 0;
 }
